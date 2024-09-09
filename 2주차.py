144a0a887f0db0c32bb04047a62d624db5fc98f3
강서영 <seoyeong6992@gmail.com>
2024-09-09 22:53:27 +0900

    tori

---
+++ 2주차.py
@@ -0,0 +1,31 @@
+# #실습 예제
+# print("Hello World!")
+# A = 7
+# B = 4
+
+# print("20210262 강서영 ")
+# print("넓이는", A*B, "입니다.")
+# print("둘레는",2*(A+B), "입니다.")
+
+# S = 0
+# for i in range(1, 11):
+#     S += i
+# print(S)
+
+# s = 0
+# for i in range(1, 11):
+#     if i % 2 == 0:
+#         s += i
+
+# print(s)
+
+
+Name = input('이름을 입력하세요 : ')
+A = int(input("나이를 입력하세요 : "))
+N = int(input("학번을 입력하세요 : "))
+
+print(Name,A,N)
+
+
+    
+
---
+++ CAS_PYTHON과제.py
@@ -0,0 +1,23 @@
+#과제 1번
+YEAR = int(input('연도를 입력하세요 : '))
+
+if YEAR % 400 == 0:
+    print("윤년입니다.")
+elif YEAR % 100 == 0:
+    print("평년입니다.")
+elif YEAR % 4 == 0:
+    print("윤년입니다.")
+else:
+    print("평년입니다.")
+
+
+#과제 2번
+N = int(input("임의의 숫자를 입력하시요 : "))
+P = [0, 1]
+
+for i in range(0, N):
+    P.append(P[i] + P[i + 1])
+
+print(P)
+
+print(P[N])
