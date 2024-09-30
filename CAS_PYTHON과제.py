8ceeb0a80e9fe0c0ef5e7004e06266b6e9e06d5d
강서영 <seoyeong6992@gmail.com>
2024-09-30 13:38:39 +0900

    tori

--- CAS_PYTHON과제.py
+++ CAS_PYTHON과제.py
@@ -15,7 +15,7 @@ else:
 N = int(input("임의의 숫자를 입력하시요 : "))
 P = [0, 1]
 
-for i in range(1, N):
+for i in range(1, N - 1):
     P.append(P[i] + P[i - 1])
-
-print(P[N])
+    
+print(P[-1])
---
+++ week3_before.py
@@ -0,0 +1,21 @@
+S = [0xc, 0x5, 0x6, 0xb, 0x9, 0x0, 0xa, 0xd, 0x3, 0xe, 0xf, 0x8, 0x4, 0x7, 0x1, 0x2]
+
+def sbox(input):
+    return 
+
+def permutation(list):
+    per_list = [list[], list[], list[], list[], list[], list[], list[], list[]]
+    return per_list
+
+def input_to_binary_list(input):
+
+    binary_str = format(input, '08b')
+    binary_list = [int(bit) for bit in binary_str]
+    
+    return binary_list
+
+def encryption(plaintext, key):
+    return cipher
+
+ciphertext = encryption(0x6b,0xff)
+print(hex(ciphertext))  
